38a665cab173aba199ad4c86398130812dcc6e35
manureini <1554111+manureini@users.noreply.github.com>
2022-11-03 00:49:47 +0100

    add Index Attribute

---
+++ MShared/MShared/IndexAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MShared
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public class IndexAttribute : Attribute
+    {
+        public bool IsUnique { get; protected set; }
+
+        public string[] PropertyNames { get; set; }
+
+        public IndexAttribute(params string[] propertyNames) : this(false, propertyNames)
+        {
+        }
+
+        public IndexAttribute(bool isUnique, params string[] propertyNames)
+        {
+            IsUnique = isUnique;
+            PropertyNames = propertyNames;
+        }
+    }
+}
